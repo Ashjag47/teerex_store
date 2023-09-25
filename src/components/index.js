02f8df64b98f4404e8dd2297fb3536f3bda2b6e3
Ashjag47 <ashjagmk@gmail.com>
2023-09-25 10:55:33 +0530

    Chore: Initial project plan and setup.

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as CartItemCard } from "./CartItemCard/CartItemCard";
+export { default as ProductCard } from "./ProductCard/ProductCard";
+export { default as FilterPipeline } from "./FilterPipeline/FilterPipeline";
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as SearchBar } from "./SearchBar/SearchBar";
---
+++ src/pages/index.js
@@ -0,0 +1,2 @@
+export { default as ProductListingPage } from "./ProductListingPage/ProductListingPage";
+export { default as ShoppingCartPage } from "./ShoppingCartPage/ShoppingCartPage";
